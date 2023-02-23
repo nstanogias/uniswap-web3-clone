672f26d5bf401ac1362557067ee1d97aa705c77f
nstanogias <nstanogias@gmail.com>
2023-02-23 12:20:09 +0100

    added connectwallet component

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,33 +1,20 @@
-import React, { useEffect } from 'react';
-import toast, { Toaster } from 'react-hot-toast';
 import { useApiContext } from '../context';
-import CustomButton from './CustomButton';
+import { ConnectWallet } from '@thirdweb-dev/react';
+import Loader from './Loader';
 
 const Header = () => {
-  const { connect, address, disconnect } = useApiContext();
-  const notifyConnectWallet = () => toast.error('Connect wallet.', { duration: 4000 });
-
-  useEffect(() => {
-    if (!address) notifyConnectWallet();
-  }, [address]);
+  const { address, balance, isLoading } = useApiContext();
 
   return (
     <div className='fixed top-0 left-0 flex items-center justify-between w-full px-8 py-4'>
+      {isLoading && <Loader />}
       <div className='flex items-center'>
         <img src='./logo.png' className='h-12' />
       </div>
+      {address && <div className='flex items-center text-white'>VOLFI balance: {balance}</div>}
       <div className='flex'>
-        <CustomButton
-          title={address ? 'Disconnect' : 'Connect'}
-          styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
-          handleClick={() => {
-            if (address) disconnect();
-            else connect();
-          }}
-        />
+        <ConnectWallet />
       </div>
-
-      <Toaster />
     </div>
   );
 };
